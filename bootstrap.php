2d191340c1029c3c50e93e81ebcadfe65dd158c6
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-11-08 21:08:02 -0700

    added sleeping when docker is first setup
    this gives time for rtorrent/rutorrent to become up and running for tests to process

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,11 @@ include "vendor/autoload.php";
 
 \Dotenv\Dotenv::create(__DIR__)->load();
 
-// Load .env
-// Bootstrap in docker
+$n = hrtime(true);
 passthru('docker-compose --no-ansi up -d');
+$p = (hrtime(true) - $n) / 1000000000;
+print 'Docker Up: '.number_format($p, 3).' (s)'.PHP_EOL;
+if($p > 1.6){
+    print '| Sleeping 20 seconds for rutorrent to become up (if script didn\'t sleep long enough just re-run test the docker will be up at that point).'.PHP_EOL;
+    sleep(20);
+}
