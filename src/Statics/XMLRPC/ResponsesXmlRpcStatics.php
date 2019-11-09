54ca286d6339a1ffa7101e94eff49d327b906228
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-11-08 19:59:03 -0700

    setup statics for parsing data

---
+++ src/Statics/XMLRPC/ResponsesXmlRpcStatics.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Voltsonic\rTorrent\Statics\XMLRPC;
+
+class ResponsesXmlRpcStatics {
+    const STANDARD_ARRAY = ['methodResponse', 'params', 'param', 'value', 'array', 'data', 'value'];
+}
