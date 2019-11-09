33ec7276dfc505972311993a07eafb05f2ddbdab
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-11-08 20:00:51 -0700

    setup views.list

--- src/XMLRPC/Commands/ViewsCommandsXmlRpc.php
+++ src/XMLRPC/Commands/ViewsCommandsXmlRpc.php
@@ -2,7 +2,18 @@
 
 namespace Voltsonic\rTorrent\XMLRPC\Commands;
 
+use ErrorException;
 use Voltsonic\rTorrent\XMLRPC\rTorrentXmlRpcAbstract;
 
 class ViewsCommandsXmlRpc extends rTorrentXmlRpcAbstract {
+    protected $methodPrefix = 'views';
+
+    /**
+     * @param callable $callbackMethod
+     * @param bool $disableStream
+     * @throws ErrorException
+     */
+    public function list(callable $callbackMethod, $disableStream = false){
+        $this->runArray($this->cmd('list'), $callbackMethod, [], $disableStream);
+    }
 }
