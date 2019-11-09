31ed0658f093ed9aa4296f30047ee5be65ba4ef9
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-11-08 21:31:36 -0700

    added static for single return call

--- src/Statics/XMLRPC/ResponsesXmlRpcStatics.php
+++ src/Statics/XMLRPC/ResponsesXmlRpcStatics.php
@@ -4,4 +4,5 @@ namespace Voltsonic\rTorrent\Statics\XMLRPC;
 
 class ResponsesXmlRpcStatics {
     const STANDARD_ARRAY = ['methodResponse', 'params', 'param', 'value', 'array', 'data', 'value'];
+    const STANDARD_SINGLE = ['methodResponse', 'params', 'param', 'value'];
 }
