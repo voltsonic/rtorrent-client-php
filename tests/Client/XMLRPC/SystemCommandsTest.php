299c8e9fc7cfb0bcfe87be35adac424bb8f645af
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-11-08 21:32:18 -0700

    added tests for shutdown quick/normal

--- tests/Client/XMLRPC/SystemCommandsTest.php
+++ tests/Client/XMLRPC/SystemCommandsTest.php
@@ -6,15 +6,39 @@ namespace VoltsonicTests\Client\XMLRPC;
 use ErrorException;
 use PHPUnit\Framework\TestCase;
 use VoltsonicTests\Traits\ClientBuilderTrait;
+use VoltsonicTests\Traits\ClientRebootTrait;
 
 final class SystemCommandsTest extends TestCase {
     use ClientBuilderTrait;
+    use ClientRebootTrait;
+
+    /**
+     * @throws ErrorException
+     */
+    public function testShutdownQuick(){
+        $completeCode = false;
+        $this->torrentClient()->commands_System
+            ->shutdown_Quick(function($complete) use(&$completeCode) { $completeCode = $complete; });
+        $this->assertTrue($completeCode == 0, 'Code: '.$completeCode);
+        $this->torrentReboot();
+    }
+
+    /**
+     * @throws ErrorException
+     */
+    public function testShutdownNormal(){
+        $completeCode = false;
+        $this->torrentClient()->commands_System
+            ->shutdown_Normal(function($complete) use(&$completeCode) { $completeCode = $complete; });
+        $this->assertTrue($completeCode == 0, 'Code: '.$completeCode);
+        $this->torrentReboot();
+    }
 
     /**
      * @param string[] $checkCommands
      * @throws ErrorException
      */
-    public function testSystemListMethods(
+    public function testListMethods(
         $checkCommands = [
             'system.listMethods',
             'd.completed_chunks',
@@ -32,11 +56,9 @@ final class SystemCommandsTest extends TestCase {
                 });
                 $ct++;
             });
-        if(!empty($checkCommands)){
-            foreach($checkCommands as $command){
-                $this->fail('Missing "'.$command.'" command in list.');
-            }
-        }
         $this->assertGreaterThanOrEqual(846, $ct, 'Methods list is shorter then expected.');
+        if(!empty($checkCommands))
+            foreach($checkCommands as $command)
+                $this->fail('Missing "'.$command.'" command in list.');
     }
 }
