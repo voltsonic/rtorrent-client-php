e12af20bd88be5f8073d67f7ebc00e424d314877
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-11-11 23:10:29 -0700

    breaking up keys - allows for easier piecing when generating the streamers

--- src/Statics/XMLRPC/ResponsesXmlRpcStatics.php
+++ src/Statics/XMLRPC/ResponsesXmlRpcStatics.php
@@ -3,6 +3,8 @@
 namespace Voltsonic\rTorrent\Statics\XMLRPC;
 
 class ResponsesXmlRpcStatics {
-    const STANDARD_ARRAY = ['methodResponse', 'params', 'param', 'value', 'array', 'data', 'value'];
-    const STANDARD_SINGLE = ['methodResponse', 'params', 'param', 'value'];
+    const COMMAND_CORE = ['methodResponse', 'params', 'param', 'value'];
+
+    const STANDARD_ARRAY = ['array', 'data', 'value'];
+    const STANDARD_SINGLE = [];
 }
